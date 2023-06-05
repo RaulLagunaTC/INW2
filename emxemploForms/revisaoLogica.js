f4ee1a245e7789d1b0199a5debf5eed0ac1546ca
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-05 12:19:08 -0300

    RevisaoLógica

---
+++ emxemploForms/revisaoLogica.js
@@ -0,0 +1,31 @@
+//console.log("Oi\nmundo")
+
+/*
+console.log("Nome\tNota\tTurma")
+console.log("Pedro\t10\t2A")
+console.log("Maria\t8\t2B")
+console.log("Anastacio\t8,5\t2C")
+
+
+let nome = "Pedro"
+console.log("Oi meu amigo"+nome)
+
+
+let nome = "Pedro"
+let valor1 = 10
+let valor2 = 20
+let valor3 = valor1 + valor2
+
+nome = "Marcio"
+console.log("Oi meu amigo(a)"+nome.toLowerCase())
+console.log("A soma dos dois valores é " (valor1+valor2))
+
+
+const leia = require("prompt-sync")()
+let numero1, numero2 = 0
+
+numero = leia("Digite o primeiro número:")
+numero = leia("Digite o segundo número:")
+
+console.log("A soma do"+numero1+"mais"+numero2+"é"+(numero1+numero2))
+*/
